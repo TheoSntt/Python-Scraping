57881d7556d9a21ccd340ab45c47ccb38bd42799
Theo Sinatti <sinatti.theo@gmail.com>
2022-11-22 17:20:23 +0100

    Step 3 & 4 completed. First working version. Needs to be cleaned, refactored, ...

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ from word2number import w2n
 # First define the URL
 theUrl = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 
-
+"""
 # Function that sends the requests to the URL, calls the function that write the csv file with the answer
 def send_request_to_book_page(url):
     response = requests.get(url)
@@ -32,35 +32,40 @@ def write_csv_file(soup, url):
         file.write(header_str)
         # Writing the infos
         file.write((format_info(soup, url)))
+"""
 
 
 def format_info(soup, url):
     info_string = ""
     # The URL is already known, it doesn't need to be scraped from the page
-    info_string += '"' + url + '",'
+    info_string += url + ','
 
     # The title can be recovered through the h1 section
-    info_string += '"' + soup.find('h1').text.replace('"', '""') + '",'
+    info_string += '"' + soup.find('h1').text.replace('"', """""") + '",'
 
     # For the description, the div is not named and doesn't have class. So we access it by finding the previous div
     # and then using the function findNext of BeautifulSoup
-    info_string += '"' + soup.find(id="product_description").findNext('p').text.replace('"', '""') + '",'
+    if soup.find(id="product_description") is not None:
+        info_string += '"'\
+            + soup.find(id="product_description").findNext('p').text.replace(' ...more', '').replace('"', """""") + '",'
+    else:
+        info_string += '"No description",'
 
     # The UPC, prices, and number available are stored in a table through which we can iterate
     tds = soup.findAll('td')
-    info_string += '"' + tds[0].text + '",'
+    info_string += tds[0].text + ','
     # For both prices value, removing the pound sign and converting to float
     price_inc = tds[3].text[1:]
     info_string += '"' + price_inc.replace(".", ",") + '",'
     price_excl = tds[3].text[1:]
     info_string += '"' + price_excl.replace(".", ",") + '",'
     # Replace statements to remove unwanted information
-    info_string += '"' + tds[5].text.replace("In stock (", "").replace(" available)", "") + '",'
+    info_string += tds[5].text.replace("In stock (", "").replace(" available)", "") + ','
 
     # The category is only visible in a link on top of the page. We can access it by finding all 'li' elements
     liz = soup.findAll('li')
     # Some blank spaces and \n where present. Removed them with the strip method.
-    info_string += '"' + liz[2].text.strip() + '",'
+    info_string += liz[2].text.strip() + ','
 
     # The review rating is found in the name of the div containing the stars
     # Getting the div
@@ -69,17 +74,20 @@ def format_info(soup, url):
     rating_text = rating_div["class"][1]
     # Converting it to number
     rating_nb = w2n.word_to_num(rating_text)
-    info_string += '"' + str(rating_nb) + '",'
+    info_string += str(rating_nb) + ','
 
     # There is only one image in the page, so we can acess the link of the image by searching
     # the one img div with bs4
     raw_link = soup.find('img')['src']
     final_link = "http://books.toscrape.com/" + raw_link[6:]
-    info_string += '"' + final_link + "\n"
+    info_string += final_link + '\n'
+
+    # Retrieving the book name from the URL to name the saved image
+    img_name = url.split("/")[-2].split('_')[0]
 
     # Returning our final object
     print(info_string)
-    return info_string
+    return info_string, final_link, img_name
 
 
 # send_request_to_book_page(theUrl)
--- script_2.py
+++ script_2.py
@@ -18,41 +18,51 @@ def send_request(url):
         soup = BeautifulSoup(response.text, "html.parser")
         return soup
     else:
-        print("La requête a échoué")
+        print(response)
 
 
-def determine_number_of_page(soup):
+def get_books_url(soup, file_name):
     # print(soup)
+    books_urls = []
     nb_pages = math.ceil(int(soup.find('form').findChildren()[1].text)/20)
     print("Number of pages for this category : " + str(nb_pages))
-    return soup, nb_pages
+    # Extract books URL of the first pages of result anyway
+    extract_books_urls(soup,file_name)
+    # If there are more than one page, get the books URL from the other pages
+    if nb_pages > 1:
+        for nb in range(2, nb_pages + 1):
+            nxt_page_url = theURL[:-10] + "page-" + str(nb) + ".html"
+            extract_books_urls(send_request(nxt_page_url), file_name)
 
 
-def extract_urls(soup):
+def write_a_book_in_csv(book_url, file_name):
+    with open('results/' + file_name + '.csv', 'w', encoding='utf-8') as file:
+        file.write(format_info(send_request(book_url), book_url))
+
+
+def extract_books_urls(soup, file_name):
     raw_url_list = soup.find('ol', {'class': 'row'}).findChildren("h3")
     for raw_url in raw_url_list:
         url = "http://books.toscrape.com/catalogue" + raw_url.findChildren("a")[0]['href'][8:]
+        write_a_book_in_csv(format_info(send_request(url), url), file_name)
         # print(url)
-        url_list.append(url)
 
 
-def get_books_url(soup):
-    extract_urls(soup)
-    if nb_page > 1:
-        for nb in range(2, nb_page+1):
-            nxt_page_url = theURL[:-10] + "page-"+ str(nb) + ".html"
-            extract_urls(send_request(nxt_page_url))
-        return url_list
+def write_file(list_url):
+    with open('results/test.csv', 'w', encoding='utf-8') as file:
+        # Writing the header
+        header_str = "product_page_url, title, product_description, universal_product_code, price_including_tax, price_excluding_tax, number_available, category, review_rating, image_url\n"
+        file.write(header_str)
+        for i in list_url:
+            file.write(format_info(send_request(i), i))
+            # print(i)
 
 
 url_list = []
+"""
 content, nb_page = determine_number_of_page(send_request(theURL))
 get_books_url(content)
+write_file(url_list)
+"""
 
-with open('results/test.csv', 'w', encoding='utf-8') as file:
-    # Writing the header
-    header_str = "product_page_url, title, product_description, universal_product_code, price_including_tax, price_excluding_tax, number_available, category, review_rating, image_url\n"
-    file.write(header_str)
-    for i in url_list:
-        file.write(format_info(send_request(i), i))
-        # print(i)
+
